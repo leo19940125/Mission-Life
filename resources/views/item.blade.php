bbaf9523102fb560549edadc41392e4146d077ea
leo19940125 <leo19940125@gmail.com>
2016-10-07 11:57:03 +0800

    商店加一些東西

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -21,11 +21,33 @@
 			<div class="row">
 				<ol class="breadcrumb">
 					<li><a href="{{ url('/') }}">首頁</a></li>
-					<li class="active">任務大廳</li>
+					<li class="active">商店</li>
 				</ol>
 			</div>
 		</div>
 	@endif
+		<div class="container">
+			<h1>Test Lab</h1>
+			<ul class="nav nav-tabs nav-justified">
+				<li class="active"><a data-toggle = "tab" href="#item1">item1</a></li>
+				<li><a data-toggle = "tab" href="#item2">item2</a></li>
+				<li><a data-toggle = "tab" href="#item3">item3</a></li>
+			</ul>
+			<div class="tab-content">
+				<div id="item1" class="tab-pane fade">
+					<h3>item1</h3>
+					<p>I'm item1</p>
+				</div>
+				<div id="item2" class="tab-pane fade">
+					<h3>item2</h3>
+					<p>Hello world!</p>
+				</div>
+				<div id="item3" class="tab-pane fade">
+					<h3>item3</h3>
+					<p>Are you?</p>
+				</div>
+			</div>
+		</div>
 
 
 @endsection
